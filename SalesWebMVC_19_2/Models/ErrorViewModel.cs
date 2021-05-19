28e74a51592f75316a39c629c1170c0c5030df14
Carlos Dourado <cadkrav10@gmail.com>
2021-05-19 17:12:57 -0300

    first commit

---
+++ SalesWebMVC_19_2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMVC_19_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
